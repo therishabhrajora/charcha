89da28d37c7ccd41b821fc449efcfc180aada785
Rishabh Rajora <rishabhrajora20@gmail.com>
2025-08-17 03:58:55 +0530

    .

--- frontend/src/component/JoinCreateChat.jsx
+++ frontend/src/component/JoinCreateChat.jsx
@@ -129,7 +129,7 @@ function JoinCreateChat() {
             </div>
           </div>
         </div>
-        <div className="capitalize text-shadow-indigo-200 font-light">Developed By:Rishabh Rajora</div>
+        <div className="capitalize text-indigo-200 font-light text-center text-xs">Developed By:Rishabh Rajora</div>
       </div>
     </>
   );
