734164c7fc5ef0b5e4cc8244b96fbe7245932d54
Rishabh Rajora <rishabhrajora20@gmail.com>
2025-08-17 02:52:14 +0530

    update rendor url

--- charcha/src/main/java/com/charcha/charcha/config/AppConstants.java
+++ charcha/src/main/java/com/charcha/charcha/config/AppConstants.java
@@ -3,5 +3,5 @@ package com.charcha.charcha.config;
 public class AppConstants {
     public static final String FRONTNED_BASE_URL_LOCAL="http://localhost:5173";
     public static final String FRONTNED_BASE_URL_NETLIFY="https://charchabot.netlify.app";
-    public static final String FRONTNED_BASE_URL_RENDER="https://charchabot.onrender.com/";
+    public static final String FRONTNED_BASE_URL_RENDER="https://charchabot.onrender.com";
 }
