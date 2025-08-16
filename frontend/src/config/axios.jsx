b8ff5c3d853a5fe96434b202c6ee2f49ed9e10c4
Rishabh Rajora <rishabhrajora20@gmail.com>
2025-08-17 02:59:49 +0530

    update backend url

--- frontend/src/config/axios.jsx
+++ frontend/src/config/axios.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 // export const baseURl = "http://localhost:8080";
-export const baseURl = "https://charcha-68tw.onrender.com/";
+export const baseURl = "https://charcha-68tw.onrender.com";
 
 export const httpClient = axios.create({
   baseURL: baseURl,
